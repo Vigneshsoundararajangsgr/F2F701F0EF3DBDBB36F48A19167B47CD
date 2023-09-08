c980d6939a635260e9dfc359c354621477d6e6a1
Vigneshsoundararajangsgr <144310785+Vigneshsoundararajangsgr@users.noreply.github.com>
2023-09-08 13:52:17 +0530

    Update challenge1_2.py

--- challenge1_2.py
+++ challenge1_2.py
@@ -1,10 +1,6 @@
 year = int(input("Enter a year: "))
 
-if year % 400 == 0:
-    print(f"{year} is a leap year")
-elif year % 100 == 0:
-    print(f"{year} is not a leap year")
-elif year % 4 == 0:
-    print(f"{year} is a leap year")
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
 else:
-    print(f"{year} is not a leap year")
+    print(year, "is not a leap year.")
